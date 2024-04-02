23c3a4c112f6b7bf667874b3999c1f0cd51c06dd
JulieKodehode <julie.kodehode@gmail.com>
2024-04-02 12:03:00 +0200

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Hello!');
+
+// HENT
+const hamburgerIcon = document.querySelector("#hamburgerIcon");
+console.log(hamburgerIcon);
+
+// MODIFISER
+function hamburgerMenu() {
+    console.log("Clicked?")
+
+    // HENT
+    const nav = document.querySelector("#nav");
+    console.log(nav);
+
+    // MODIFISER
+    nav.classList.toggle("headerHamburger");
+}
+
+// SEND
+hamburgerIcon.addEventListener("click", hamburgerMenu)
